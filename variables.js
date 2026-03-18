5670034dd57b4bff5d4248091dcf58fab33cf0a4
otoneumann <otarshenge@gmail.com>
2026-03-19 00:08:44 +0100

    Variables

---
+++ variables.js
@@ -0,0 +1,26 @@
+let favouriteFruit  = 'strawberry' ;
+let favouriteCream  = "chocolate";
+let favourite  = `JavaScript`;
+
+let numberOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNumber = 5454542552356262n;
+
+let lovesCoding = true;
+
+let favouriteColor;
+
+
+console.log(favouriteColor);
+
+favouriteFruit = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+    name: "JavaScript",
+    hours: 3
+};
+
+
