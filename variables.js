649725e6aee7bb60de62f3465a79e3df1badf653
otoneumann <otarshenge@gmail.com>
2026-03-24 14:45:47 +0100

    basics

--- variables.js
+++ variables.js
@@ -1,3 +1,96 @@
+
+
+/*
+
+
+let a = null;
+
+const b = a ?? false;
+
+console.log(b);
+
+let a = true;
+
+const b = !a;
+
+console.log(b);
+
+let a = true;
+let b = true;
+let c = true;
+let d = true;
+
+
+console.log(a && b || c && d);
+console.log((a && b) || (c && d) || (c && d));
+
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+console.log(false && false);
+
+console.log(true || true);
+console.log(false || true);
+console.log(true || false);
+console.log(false || false);
+
+let points = 110;
+
+const type = points > 100 ?'gold' : 'silver';
+
+console.log(type);
+
+let age = 16;
+
+const drive  = age >= 16 ? true : false;
+
+console.log(drive);
+
+let a = 2;
+let b = 3;
+
+console.log(a === b);
+
+
+
+let num1 = 5;
+let num2 = 2;
+
+const isNumGreater=num1>num2;
+
+console.log(isNumGreater);
+
+
+function multiply (num1, num2) {
+    const product=  num1 * num2;
+}
+
+console.log(multiply(2,2));s
+
+function sayHi(name) {
+    console.log('hi! ' + name);
+}
+
+sayHi('steven');
+
+let productColors = ['blue', 'green', 'red', 'blue', 'purple'];
+
+productColors[0] = 242;
+
+console.log(typeof productColors.length);
+
+let course = {
+    name: 'course',
+    hours: 3
+};
+
+let property = 'hours';
+
+
+console.log(course[property]);
+
+
+
 let favouriteFruit  = 'strawberry' ;
 let favouriteCream  = "chocolate";
 let favourite  = `JavaScript`;
@@ -23,4 +116,11 @@ let course = {
     hours: 3
 };
 
+let firstName = 'John';
+console.log(typeof firstName);
 
+firstName = 100;
+console.log(typeof firstName);
+
+firstName = true;
+console.log(typeof firstName);*/
