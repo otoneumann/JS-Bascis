7f725d0b1764ce67534c48d4c3e663fb198567cd
otoneumann <otarshenge@gmail.com>
2026-03-25 17:30:06 +0100

    methods

--- wokrsheet.js
+++ wokrsheet.js
@@ -1,6 +1,54 @@
 
 
+
 /*
+const
+    dog = {
+        name: "wokrsheet",
+        age: 4,
+        eye: "Hite"
+    }
+
+const keys = Object.keys(dog);
+
+console.log(keys);
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+
+for ( const elemets of num) {
+    console.log(elemets);
+}
+
+
+let a = {value: 20}
+
+let b = a;
+
+a.value = 100;
+
+console.log(a);
+console.log(b);
+let a = 10;
+let b = a;
+
+a = 20;
+
+console.log(a);
+console.log(b);
+
+
+const Programmer = new Function(
+    'name',
+    `
+    this.name = 'John'
+    `
+
+);
+
+function add(num1,num2){
+    return num1+num2;
+}
+
 
 const person = {
     name: "John",
