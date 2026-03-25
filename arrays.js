76233dcc85b7f285966380495716f62ac770df59
otoneumann <otarshenge@gmail.com>
2026-03-25 17:44:26 +0100

    arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5];
+
+num.push(7,9);
+console.log(num)
+
+num.unshift(25);
+
+
+num.splice(1,0,25,23);
+
+console.log(num);
--- objects.js
+++
@@ -1,7 +0,0 @@
-let min = 1;
-
-let max = 10;
-
-const rnd = Math.round(Math.random() * (max - min) + min);
-
-console.log(rnd);
