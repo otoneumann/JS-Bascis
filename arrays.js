76add3228dc6ed1ad2d0bfd674f98aedca70ebbf
otoneumann <otarshenge@gmail.com>
2026-03-25 21:25:12 +0100

    arrays ...

--- arrays.js
+++ arrays.js
@@ -1,8 +1,29 @@
+const a = [1, 2, 3, 4, 5];
+const b = [1, 2, 3, 4, 5];
+
+const ab = a.concat(b);
+console.log(ab);
+
+/*
+
+const num = [1, 2, 3, 4, 5];
+let anotherArray = numbers;
+
+
+
+while(num.length > 0){
+    num.pop();
+}
+
+num.length = 0;
+
+console.log(num);
+
+num.splice(0, num.length);
+
 const add = (num1, num2) => num1 + num2;
 
 console.log(add(5,2));
-
-/*
 const num = [1, 2, 3, 4, 5];
 
 const indexOfThree =  num.indexOf(3)
