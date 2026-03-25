1d3bd4e88f6e61794d8eede35fdd2319ee58dcb1
otoneumann <otarshenge@gmail.com>
2026-03-25 17:56:15 +0100

    arrow fun

--- arrays.js
+++ arrays.js
@@ -1,4 +1,15 @@
+const add = (num1, num2) => num1 + num2;
+
+console.log(add(5,2));
+
+/*
 const num = [1, 2, 3, 4, 5];
+
+const indexOfThree =  num.indexOf(3)
+
+//console.log(num[indexOfThree]);
+
+console.log(num.lastIndexOf(1))
 
 num.push(7,9);
 console.log(num)
@@ -9,3 +20,4 @@ num.unshift(25);
 num.splice(1,0,25,23);
 
 console.log(num);
+*/
